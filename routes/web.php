1ed126c2f2bbd037b5310364da9aab1ad2293754
cortejojicoy <johnjercai.cortejo@lspu.edu.ph>
2026-05-05 13:47:21 +0800

    feat(api): add base route for pdf-builder

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::prefix('pdf-builder/api')
     ->middleware(['web', 'auth'])
     ->name('pdf-builder.api.')
     ->group(function () {
+        Route::get('/',                        [PdfTemplateController::class, 'base'])->name('base');
         Route::get('/templates/{id}',          [PdfTemplateController::class, 'show'])->name('templates.show');
         Route::put('/templates/{id}',          [PdfTemplateController::class, 'update'])->name('templates.update');
         Route::delete('/templates/{id}',       [PdfTemplateController::class, 'destroy'])->name('templates.destroy');
